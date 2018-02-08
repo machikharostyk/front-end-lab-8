10a885a98d249c9b598039299447c43e8f4624c6
Rostyk Machikha <finessq@gmail.com>
2018-02-08 16:57:27 +0200

    homework_06

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,3 +1,6 @@
-var euro_number=prompt('Enter please money in euro');
-var usd_number=prompt('Enter please money in usd');
-console.log(euro_number + " EURO are equal " + euro_number*29 + " UAH, " + usd_number  + " USD are equal " + usd_number*14.04 + " UAH, one euro is equal 1.074 dollars");
+var euro_number = prompt('Enter please money in euro');
+var usd_number = prompt('Enter please money in usd');
+var euro_course = 33.4;
+var usd_course = 27.1;
+var eu_to_usd=euro_course/usd_course;
+console.log(euro_number + " EURO are equal " + euro_number * euro_course + " UAH, " + usd_number + " USD are equal " + usd_number * usd_course + " UAH, one euro is equal " + eu_to_usd.toFixed(2) + " dollars");
