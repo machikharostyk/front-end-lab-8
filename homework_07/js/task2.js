4f7549de60cd2d5a27b6a65024b97a7e5dc6df4e
Rostyk Machikha <finessq@gmail.com>
2018-03-04 15:50:39 +0200

    fixed mistakes

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -6,24 +6,27 @@ var total_prize = 0;
 var possible_prize = 10;
 
 
-function game_func() {
+
+
+
+if (game_start == true) {
 	var number = Math.floor(Math.random() * (mult_range + 1));
-	
 	var attempts = 3;
 	while (attempts !== 0 && game_end !== true) {
 		var game = prompt('Enter a number from 0 to ' + mult_range + '\nAttempts left: ' + attempts + '\nTotal prize: ' + total_prize + '$' + '\nPossible prize on current attempt: ' + possible_prize + '$');
 		if (game == number) {
+			
 			total_prize += possible_prize;
 			var continue_game = confirm('Congratulations! Do you want to continue a game?');
 
 			if (continue_game == true) {
-
+				
 				mult_range = mult_range * 2;
+				var number = Math.floor(Math.random() * (mult_range + 1));
 				possible_prize = possible_prize * mult_prize;
 				attempts = 3;
-				game_func();
+				
 			} else {
-
 				console.log('Thank you for a game your prize is: ' + total_prize + '$');
 				game_end = true;
 				break;
@@ -36,8 +39,11 @@ function game_func() {
 		if (attempts == 0) {
 			var play_again = confirm('Sorry but you lose( Do you want to play again?');
 			if (play_again == true) {
+				mult_range=5;
+				var number = Math.floor(Math.random() * (mult_range + 1));
 				possible_prize = 10;
-				game_func();
+				attempts = 3;
+				
 			} else {
 				total_prize = 0;
 				console.log('Thank you for a game your prize is: ' + total_prize + '$');
@@ -47,9 +53,6 @@ function game_func() {
 			}
 		}
 	}
-}
-if (game_start == true) {
-	game_func();
 } else {
 	console.log('You did not become a milionaire');
 }
